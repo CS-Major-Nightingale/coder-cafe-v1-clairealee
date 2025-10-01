fa518ac6c0e417d5c36a5553e4ccc7902bee5922
clairelee <clee@nightingale.org>
2025-10-01 12:18:43 -0400

    tabs

--- src/CoderCafe.java
+++ src/CoderCafe.java
@@ -28,31 +28,31 @@ public class CoderCafe {
         DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm");
         String formattedTime = myTime.format(myFormatter);
 
-        System.out.println("*************************************");
-        System.out.println("*\tWelcome to Claire's Coder Cafe!\t*");
-        System.out.println("*\t\t\t" + myDate + " " + formattedTime + "\t\t*");
+        System.out.println("*****************************************");
+        System.out.println("*\tWelcome to Claire's Coder Cafe!\t\t*");
+        System.out.println("*\t\t\t" + myDate + " " + formattedTime + "\t\t\t*");
 
         System.out.print("*\t" + icedMatchaLatteItems + " x " + "Iced Matcha Latte\t $");
-        System.out.printf("%.2f\t*\n",icedMatchaLatte*icedMatchaLatteItems);
+        System.out.printf("%.2f\t\t*\n",icedMatchaLatte*icedMatchaLatteItems);
 
         System.out.print("*\t" + icedLatteItems + " x " + "Iced Latte\t\t\t $");
-        System.out.printf("%.2f\t*\n",icedLatte*icedLatteItems);
+        System.out.printf("%.2f\t\t*\n",icedLatte*icedLatteItems);
 
         System.out.print("*\t" + cappuccinoItems + " x " + "Cappuccino\t\t\t $");
-        System.out.printf("%.2f\t*\n",cappuccino*cappuccinoItems);
+        System.out.printf("%.2f\t\t*\n",cappuccino*cappuccinoItems);
 
         System.out.print("*\t" + hotChocolateItems + " x " + "Hot Chocolate\t\t $");
-        System.out.printf("%.2f\t*\n",hotChocolate*hotChocolateItems);
+        System.out.printf("%.2f\t\t*\n",hotChocolate*hotChocolateItems);
 
-        System.out.println("*\t\t\t\t\t\t\t\t\t*");
-        System.out.printf("*\tSubtotal: $%.2f\t\t\t\t*\n", subTotal);
-        System.out.printf("*\tSalesTax: $%.2f\t\t\t\t\t*\n", tax);
-        System.out.printf("*\tService Fee: $%.2f\t\t\t\t*\n", serviceFee);
-        System.out.printf("*\tCoder cafe Fee: $%.2f\t\t\t*\n", cafeFee);
-        System.out.println("*\t\t\t\t\t\t\t\t\t*");
-        System.out.printf("*\tTotal: $%.2f\t\t\t\t\t*\n", total);
-        System.out.println("*\t\tThank you, come again!\t\t*");
-        System.out.println("*************************************");
+        System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+        System.out.printf("*\tSubtotal:\t\t\t\t $%.2f\t\t*\n", subTotal);
+        System.out.printf("*\tSalesTax: \t\t\t\t $%.2f\t\t*\n", tax);
+        System.out.printf("*\tService Fee: \t\t\t $%.2f\t\t*\n", serviceFee);
+        System.out.printf("*\tCoder cafe Fee: \t\t $%.2f\t\t*\n", cafeFee);
+        System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+        System.out.printf("*\tTotal: \t\t\t\t\t $%.2f\t\t*\n", total);
+        System.out.println("*\t\tThank you, come again!\t\t\t*");
+        System.out.println("*****************************************");
 
 
 
