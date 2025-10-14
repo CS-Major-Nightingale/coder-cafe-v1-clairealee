9aab4d52684562ec4621c775d7aa97373d2f98e2
clairelee <clee@nightingale.org>
2025-10-14 15:09:19 -0400

    corrections

--- src/CoderCafe.java
+++ src/CoderCafe.java
@@ -5,45 +5,53 @@ import java.time.format.DateTimeFormatter;
 
 public class CoderCafe {
     public static void main(String args[]) {
-        //declaring variables
-        double icedMatchaLatte = 4.50;
-        double icedLatte = 5.30;
-        double cappuccino = 3.75;
-        double hotChocolate = 4.80;
+        //declaring constant variables
+        double icedMatchaLattePrice = 4.50;
+        double icedLattePrice = 5.30;
+        double cappuccinoPrice = 3.75;
+        double hotChocolatePrice = 4.80;
+        double serviceFee = 1.21;
 
-        int icedMatchaLatteItems = 3;
-        int icedLatteItems = 2;
-        int cappuccinoItems = 4;
-        int hotChocolateItems = 2;
+        int icedMatchaLatteItems = 2;
+        int icedLatteItems = 4;
+        int cappuccinoItems = 3;
+        int hotChocolateItems = 1;
+
+        //subtotals for items
+        double icedMatchaLatteSubtotal = icedMatchaLattePrice * icedMatchaLatteItems;
+        double icedLatteSubtotal = icedLattePrice * icedLatteItems;
+        double cappuccinoSubtotal = cappuccinoPrice * cappuccinoItems;
+        double hotChocolateSubtotal = hotChocolatePrice * hotChocolateItems;
 
         //calculating variables
-        int items = (icedMatchaLatteItems + icedLatteItems + cappuccinoItems + hotChocolateItems);
-        double subTotal = (icedMatchaLatte * icedMatchaLatteItems) + (icedLatte * icedLatteItems) + (cappuccino * cappuccinoItems) + (hotChocolate * hotChocolateItems);
+        int totalItems = icedMatchaLatteItems + icedLatteItems + cappuccinoItems + hotChocolateItems;
+        double subTotal = icedMatchaLatteSubtotal + icedLatteSubtotal + cappuccinoSubtotal + hotChocolateSubtotal;
         double tax = subTotal * .08875;
-        double serviceFee = 1.21;
-        double cafeFee = subTotal % items;
+        double cafeFee = subTotal % totalItems;
         double total = tax + serviceFee + cafeFee + subTotal;
 
         //objects for time
         LocalDate myDate = LocalDate.now();
         LocalTime myTime = LocalTime.now();
-        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm::ss");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedDate = myDate.format(dateFormatter);
         String formattedTime = myTime.format(myFormatter);
 
         //printing
         System.out.println("*****************************************");
         System.out.println("*\tWelcome to Claire's Coder Cafe!\t\t*");
-        System.out.println("*\t\t " + myDate + " " + formattedTime + " \t\t\t*");
+        System.out.println("*\t\t " + formattedDate + " " + formattedTime + " \t\t\t*");
         System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
 
         System.out.print("*\t" + icedMatchaLatteItems + " x " + "Iced Matcha Latte\t $");
-        System.out.printf("%.2f\t\t*\n",icedMatchaLatte*icedMatchaLatteItems);
+        System.out.printf("%.2f\t\t*\n",icedMatchaLatteSubtotal);
         System.out.print("*\t" + icedLatteItems + " x " + "Iced Latte\t\t\t $");
-        System.out.printf("%.2f\t\t*\n",icedLatte*icedLatteItems);
+        System.out.printf("%.2f\t\t*\n",icedLatteSubtotal);
         System.out.print("*\t" + cappuccinoItems + " x " + "Cappuccino\t\t\t $");
-        System.out.printf("%.2f\t\t*\n",cappuccino*cappuccinoItems);
+        System.out.printf("%.2f\t\t*\n",cappuccinoSubtotal);
         System.out.print("*\t" + hotChocolateItems + " x " + "Hot Chocolate\t\t $");
-        System.out.printf("%.2f\t\t*\n",hotChocolate*hotChocolateItems);
+        System.out.printf("%.2f\t\t*\n",hotChocolateSubtotal);
 
         System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
         System.out.printf("*\tSubtotal:\t\t\t\t $%.2f\t\t*\n", subTotal);
